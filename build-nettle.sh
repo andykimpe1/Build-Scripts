8d6e97506b9964ff5946ed8e736f31d7acc8f28e
andykimpe1 <andykimpe@gmail.com>
2025-11-29 04:15:06 +0100

    Fix wget command to remove unnecessary options

--- build-nettle.sh
+++ build-nettle.sh
@@ -74,7 +74,7 @@ echo "**********************"
 echo ""
 echo "Nettle ${NETTLE_VER}..."
 
-if ! "${WGET}" -q -O "$NETTLE_TAR" --ca-certificate="${LETS_ENCRYPT_ROOT}" \
+if ! "${WGET}" -q -O "$NETTLE_TAR" \
      "https://ftp.gnu.org/gnu/nettle/$NETTLE_TAR"
 then
     echo "Failed to download Nettle"
