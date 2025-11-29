575a3bfbbc9d62bb679fc4d1110f9a93bba26e67
andykimpe1 <andykimpe@gmail.com>
2025-11-29 23:35:20 +0100

    Update build-ncurses.sh

--- build-ncurses.sh
+++ build-ncurses.sh
@@ -40,13 +40,11 @@ if [[ "${SUDO_PASSWORD_DONE}" != "yes" ]]; then
         exit 1
     fi
 fi
-   
-if [ -f "${INSTX_PKG_CACHE}/${PKG_NAME}" ]; then
-    ACTU_VER=$(cat "${INSTX_PKG_CACHE}/${PKG_NAME}")
-    if [ "$NCURSES_VER" == "$ACTU_VER" ]; then
-       echo "$PKG_NAME $ACTU_VER" is installed."
-       exit 0
-    fi
+
+
+if [[ -f "${INSTX_PKG_CACHE}/${PKG_NAME}" && ( "$NCURSES_VER" = "$(cat ${INSTX_PKG_CACHE}/${PKG_NAME})" ) ]] ; then
+    echo "$PKG_NAME $(cat ${INSTX_PKG_CACHE}/${PKG_NAME}) is installed."
+    exit 0
 fi
 
 ###############################################################################
