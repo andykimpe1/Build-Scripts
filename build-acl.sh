919d2aa9352f111fbf22a5f169284767bcadf4e1
andykimpe1 <andykimpe@gmail.com>
2025-11-29 07:01:42 +0100

    Update build-acl.sh

--- build-acl.sh
+++ build-acl.sh
@@ -153,25 +153,6 @@ bash "${INSTX_TOPDIR}/fix-pkgconfig.sh"
 # Fix runpaths
 bash "${INSTX_TOPDIR}/fix-runpath.sh"
 
-echo ""
-echo "**************************"
-echo "Testing package"
-echo "**************************"
-
-MAKE_FLAGS=("check" "-k" "V=1")
-if ! "${MAKE}" "${MAKE_FLAGS[@]}"
-then
-    echo ""
-    echo "**************************"
-    echo "Failed to test libacl"
-    echo "**************************"
-
-    bash "${INSTX_TOPDIR}/collect-logs.sh" "${PKG_NAME}"
-    exit 1
-fi
-
-# Fix runpaths again
-bash "${INSTX_TOPDIR}/fix-runpath.sh"
 
 echo ""
 echo "**************************"
