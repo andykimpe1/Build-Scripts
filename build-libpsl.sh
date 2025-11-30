d59f7de2f6801d041ea2c3223b424a7898113542
andykimpe1 <andykimpe@gmail.com>
2025-11-30 23:47:28 +0100

    Update build-libpsl.sh

--- build-libpsl.sh
+++ build-libpsl.sh
@@ -147,7 +147,7 @@ mkdir -p list
 #   https://publicsuffix.org/list/public_suffix_list.dat,
 #   rather than any other VCS sites. Pulling from any other
 #   URL is not guaranteed to be supported.
-if ! "${WGET}" -q -O "list/public_suffix_list.dat"O}" \
+if ! "${WGET}" -q -O "list/public_suffix_list.dat" \
      "https://publicsuffix.org/list/public_suffix_list.dat"
 then
     echo "Failed to update Public Suffix List (PSL)"
