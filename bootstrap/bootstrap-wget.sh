b83d038066fcf5d78d64c8163628429f623ad9f2
andykimpe1 <andykimpe@gmail.com>
2025-12-04 21:29:35 +0100

    Update bootstrap-wget.sh

--- bootstrap/bootstrap-wget.sh
+++ bootstrap/bootstrap-wget.sh
@@ -4,38 +4,6 @@
 # This script builds Wget, Unistring and OpenSSL from sources.
 # This Wget is crippled, but allows bootstrapping a full Wget build.
 
-if [ -z $LD_LIBRARY_PATH ]; then
-LD_LIBRARY_PATH=$HOME/.local/lib/
-export LD_LIBRARY_PATH=$HOME/.local/lib/
-echo 'LD_LIBRARY_PATH=$HOME/.local/lib/' >> $HOME/.bashrc
-echo 'export LD_LIBRARY_PATH=$HOME/.local/lib/' >> $HOME/.bashrc
-fi
-if [ -z $PKG_CONFIG_PATH ]; then
-PKG_CONFIG_PATH=$HOME/.local/lib/pkconfig/
-export PKG_CONFIG_PATH=$HOME/.local/lib/pkconfig/
-echo 'PKG_CONFIG_PATH=$HOME/.local/lib/pkconfig/' >> $HOME/.bashrc
-echo 'export PKG_CONFIG_PATH=$HOME/.local/lib/pkconfig/' >> $HOME/.bashrc
-fi
-patchcheck="$(echo $PATH|grep $HOME/.local/bin)"
-if [ -z $patchcheck ]; then
-PATH=$PATH:$HOME/.local/bin/
-export PATH=$PATH:$HOME/.local/bin/
-echo 'PATH=$PATH:$HOME/.local/bin/' >> $HOME/.bashrc
-echo 'export PATH=$PATH:$HOME/.local/bin/' >> $HOME/.bashrc
-fi
-patchcheck="$(echo $PATH|grep $HOME/.build-scripts/wget/bin)"
-if [ -z $patchcheck ]; then
-PATH=$PATH:$HOME/.build-scripts/wget/bin/
-export PATH=$PATH:$HOME/.build-scripts/wget/bin/
-echo 'PATH=$PATH:$HOME/.build-scripts/wget/bin/' >> $HOME/.bashrc
-echo 'export PATH=$PATH:$HOME/.build-scripts/wget/bin/' >> $HOME/.bashrc
-fi
-if [ -z $INSTX_PREFIX ]; then
-INSTX_PREFIX="$HOME/.local"
-export INSTX_PREFIX="$HOME/.local"
-echo 'INSTX_PREFIX="$HOME/.local"' >> $HOME/.bashrc
-echo 'export INSTX_PREFIX="$HOME/.local"' >> $HOME/.bashrc
-fi
 if [ -f /usr/bin/apt-get ]; then
    mkdir -p $HOME/.local/bin/
    cp ./apt-local $HOME/.local/bin/
