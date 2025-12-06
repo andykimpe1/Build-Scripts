41c3538b1b7494810cd1e4b8834ddd53461f7b17
andykimpe1 <andykimpe@gmail.com>
2025-12-06 23:41:24 +0100

    Update build-automake.sh

--- build/build-automake.sh
+++ build/build-automake.sh
@@ -42,26 +42,6 @@ if [[ -f "${INSTX_PKG_CACHE}/${PKG_NAME}" && ( "$VERSION" = "$(cat ${INSTX_PKG_C
     exit 0
 fi
 
-###############################################################################
-
-if ! ${INSTX_TOPDIR}/build.sh cacert
-then
-    echo "Failed to install CA Certs"
-    exit 1
-fi
-
-
-
-###############################################################################
-
-if ! ${INSTX_TOPDIR}/build.sh autoconf
-then
-    echo "Failed to build autoconf"
-    exit 1
-fi
-
-###############################################################################
-
 echo ""
 echo "========================================"
 echo "=============== Automake ==============="
