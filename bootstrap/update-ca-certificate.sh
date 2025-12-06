aca88a25806a9ffdf4d2e6609934f84f49c984d6
andykimpe1 <andykimpe@gmail.com>
2025-12-06 20:47:26 +0100

    Delete bootstrap directory

--- bootstrap/update-ca-certificate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -f /tmp/ca-certificate
-mkdir -p /tmp/ca-certificate
-cd /tmp/ca-certificate
-chmod +x $HOME/Build-Scripts/bootstrap/mk-ca-bundle.pl
-$HOME/Build-Scripts/bootstrap/mk-ca-bundle.pl
-cat ca-bundle.crt > $HOME/Build-Scripts/bootstrap/cacert2.pem
-cd
-rm -rf /tmp/ca-certificate
