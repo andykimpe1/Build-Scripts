9a5e0e4bd9340fe47f8dae4032c9586188d36bf2
andykimpe1 <andykimpe@gmail.com>
2025-12-06 05:54:20 +0100

    Update and rename build-apg.sh to build/build-apg.sh

--- build/build-apg.sh
+++ build/build-apg.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+INSTX_TOPDIR=$(find $HOME -name Build-Scripts.racine | sed "s|/Build-Scripts.racine||")
+
+if [[ ! -d "${INSTX_TOPDIR}/programs" ]]; then
+        printf "INSTX_TOPDIR is not valid."
+        [[ "$0" == "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
 # Written and placed in public domain by Jeffrey Walton
 # This script builds APG from sources. APG is treated
 # like a library rather then a program to avoid rebuilding
@@ -14,7 +21,7 @@ PKG_NAME=apg
 
 # Get the environment as needed.
 if [[ "${SETUP_ENVIRON_DONE}" != "yes" ]]; then
-    if ! source ./setup-environ.sh
+    if ! source ${INSTX_TOPDIR}/setup-environ.sh
     then
         echo "Failed to set environment"
         exit 1
@@ -29,7 +36,7 @@ fi
 
 # The password should die when this subshell goes out of scope
 if [[ "${SUDO_PASSWORD_DONE}" != "yes" ]]; then
-    if ! source ./setup-password.sh
+    if ! source ${INSTX_TOPDIR}/setup-password.sh
     then
         echo "Failed to process password"
         exit 1
@@ -38,7 +45,7 @@ fi
 
 ###############################################################################
 
-if ! ./build-cacert.sh
+if ! ${INSTX_TOPDIR}/build/build-cacert.sh
 then
     echo "Failed to install CA Certs"
     exit 1
@@ -74,13 +81,13 @@ cd "$APG_DIR" || exit 1
 #cp apg.c apg.c.orig
 
 # Patches are created with 'diff -u' from the pkg root directory.
-if [[ -e ../patch/apg.patch ]]; then
+if [[ -e .${INSTX_TOPDIR}/patch/apg.patch ]]; then
     echo ""
     echo "***********************"
     echo "Patching package"
     echo "***********************"
 
-    patch -u -p0 < ../patch/apg.patch
+    patch -u -p0 < .${INSTX_TOPDIR}/patch/apg.patch
 fi
 
 echo ""
