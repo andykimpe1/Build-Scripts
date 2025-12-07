fbc80fc97cc443e5d4a59126045f0f8e301808b6
andykimpe1 <andykimpe@gmail.com>
2025-12-07 01:35:29 +0100

    Update build-base.sh

--- build/build-base.sh
+++ build/build-base.sh
@@ -141,7 +141,7 @@ hash -r
 ###############################################################################
 if [ ! -f "${INSTX_PKG_CACHE}/make" ]; then
 
-if ! ${INSTX_TOPDIR}/build.sh make.sh 4.2.1
+if ! VERSION=3.81 ${INSTX_TOPDIR}/build.sh gmake381 3.81
 then
     echo "Failed to build make 4.2.1"
     exit 1
